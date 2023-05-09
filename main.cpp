f454fe3a8ddeb564bd56981db260483337913018
Jacob Lum <lumjaco@myvuw.ac.nz>
2023-05-10 11:48:36 +1200

    Added a function to determine if a pixel is black

--- main.cpp
+++ main.cpp
@@ -31,6 +31,22 @@ void moveForward() {
 
 }
 
+bool isBlack(int row, int col) {
+    /**
+     * This function takes the row and column of the desired pixel as integers.
+     * Then it determines if the pixel is black and returns True if it is and False if it isn't
+     */
+    const int MAX_INTENSITY = 77;
+    // Find each colour to determine if any are too bright
+    int red = (int)get_pixel(row, col, 0);
+    int green = (int)get_pixel(row, col, 1);
+    int blue = (int)get_pixel(row, col, 2);
+    // Check if the pixels have too much colour
+    if (red > MAX_INTENSITY || green > MAX_INTENSITY || blue > MAX_INTENSITY) return false;
+    return true;
+
+}
+
 int main() {
     int err;
     err = init(0);
