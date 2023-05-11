20ec8a19810bd4fe3f650597046256daa3937960
Jacob Lum <lumjaco@myvuw.ac.nz>
2023-05-11 13:41:22 +1200

    Added isRed to isColour.h

--- isColour.h
+++ isColour.h
@@ -29,3 +29,19 @@ bool isGreen(int row, int col) {
     if (green < MIN_GREEN || green/unwanted_divisor < red || green/unwanted_divisor < blue) return false;
     return true;
 }
+
+bool isRed(int row, int col) {
+    /**
+     * This function takes the row and column of the desired pixel as integers.
+     * Then it determines if the pixel is red and returns True if it is and False if it isn't
+     */
+    const double unwanted_divisor = 2.1;
+    const int MIN_RED = 50;
+    // Find each colour to determine if any are too bright
+    int red = (int)get_pixel(row, col, 0);
+    int green = (int)get_pixel(row, col, 1);
+    int blue = (int)get_pixel(row, col, 2);
+    // Check if the pixels have too much colour
+    if (red < MIN_RED || red/unwanted_divisor < green || red/unwanted_divisor < blue) return false;
+    return true;
+}
