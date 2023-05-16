c8dc1361077cc6ce579e1d9701bd5eb77d37d529
Jacob Lum <lumjaco@myvuw.ac.nz>
2023-05-17 10:27:03 +1200

    Made isOrange function But it will require calibration

--- isColour.h
+++ isColour.h
@@ -45,3 +45,20 @@ bool isRed(int row, int col) {
     if (red < MIN_RED || red/unwanted_divisor < green || red/unwanted_divisor < blue) return false;
     return true;
 }
+
+bool isOrange(int row, int col) {
+    /**
+     * This function takes the row and column of the desired pixel as integers.
+     * Then it determines if the pixel is red and returns True if it is and False if it isn't
+     * This function will likely require calibration to work
+     */
+    const double unwanted_divisor = 2.1;
+    const int MIN_RED = 50;
+    // Find each colour to determine if any are too bright
+    int red = (int)get_pixel(row, col, 0);
+    int green = (int)get_pixel(row, col, 1);
+    int blue = (int)get_pixel(row, col, 2);
+    // Check if the pixels have too much colour
+    if (red < MIN_RED || green > red || green < MIN_RED/unwanted_divisor || red/unwanted_divisor < blue) return false;
+    return true;
+}
