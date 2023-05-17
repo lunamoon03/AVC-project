d98f878555f5929713e19104e2a82a750b2ce707
Luna Wilkes <luna.wilkes@gmail.com>
2023-05-17 14:59:07 +1200

    hopefully fixed segmentation error

--- main.cpp
+++ main.cpp
@@ -32,27 +32,11 @@ void stop_motors() {
 }
 
 void move_forward(int time) {
-    set_motors(1, 30);
+    set_motors(1, 31);
     set_motors(3, 65);
     hardware_exchange();
     sleep(time);
     stop_motors();
-}
-
-// returns an int vector where each value is the index of that value minus the index of the middle value
-void  generate_error_vec(int size, std::vector<int>& v) {
-    // init vector and fill using iota
-    std::iota(v.begin(), v.end(), 0);
-    
-    // create iterator and find mid value
-    std::vector<int>::iterator i;
-    int mid = v[v.size()/2];
-    
-    // subtract mid from each value
-    for (i = v.begin(); i < v.end(); i++) {
-	*i -= mid;
-    }
-    
 }
 
 // get vector of center line black pixels for quad2
@@ -68,13 +52,13 @@ void get_center_line(int row, int cols, std::vector<int>& line) {
 
 // multiplies all values in one vector by their corresponding value in another vector
 void convert_line_to_error(std::vector<int>& line, std::vector<int>& error_vec) {
-    for (int i = 0; i < line.size(); i++) {
-	line[i] *= error_vec[i];
+    for (unsigned int i = 0; i < line.size(); i++) {
+	    line[i] *= error_vec[i];
     }
 }
 
 // returns how far off the centre the black line is for quad2
-int get_quad1_error(int img_height, int img_width, std::vector<int>& error_vec) {
+int get_quad2_error(int img_height, int img_width, std::vector<int>& error_vec) {
     //dont necessarily want to be making a new error vector every time - may make parameter
     
     std::vector<int> line;
@@ -83,12 +67,14 @@ int get_quad1_error(int img_height, int img_width, std::vector<int>& error_vec) 
     return std::accumulate(line.begin(), line.end(), 0);
 }
 
-void quad1() {
+void quad2() {
     // set up unchanging quad1 fields
     int img_height = 240;
     int img_width = 320;
-    std::vector<int> error_vec;
-    generate_error_vec(img_width, error_vec);
+    //create error vec -160 to 159 values
+    std::vector<int> error_vec(img_width);
+    std::iota(error_vec.begin(), error_vec.end(), img_width/2);
+    
     int v_go_r = 42; // 6 less than midpoint
     int v_go_l = 54; // 6 more than midpoint
     int tick = 100; //ms
@@ -108,6 +94,7 @@ void quad1() {
     set_motors(3, v_r);
     hardware_exchange();
     
+    std::cout<<"start scanning"<<std::endl;
     while (true) {
 	// turn proportional to error while still going forward
 	// adjust speed of appropriate wheel - assume 1 is left and 3 is right
@@ -116,18 +103,12 @@ void quad1() {
 	// adjustment = Kp*error + Kd*de/dt
 	// adjustment = difference between left and right speeds
 	take_picture();
-	curr_error = get_quad1_error(img_height, img_width, error_vec);
+	curr_error = get_quad2_error(img_height, img_width, error_vec);
 	err_delta = (curr_error-prev_error)/tick_sec;
 	delta_vel = Kp * curr_error + Kd * err_delta;
 	
-	std::cout<<"current error: "<<curr_error<<std::endl;
-	std::cout<<"velocity delta: "<<delta_vel<<std::endl<<std::endl;
-	
 	v_l = v_go_l + delta_vel;
 	v_r = v_go_r + delta_vel;
-	
-	std::cout<<"left: "<<v_l<<std::endl<<std::endl;
-	std::cout<<"right: "<<v_r<<std::endl<<std::endl;
 	
 	set_motors(1, v_l);
 	set_motors(3, v_r);
@@ -163,11 +144,11 @@ int main() {
 
     // quad1
     open_gate();
-
+    
     //move forward for an amount of time until reach quad2
 
     // quad2
-
+    quad2();
     // quad 3
 
     // quad4
